fabd2b96f56bb73acd5a8fa410f9a502fb3d6e0d
shahramteimoory <Darn.shan@yahoo.com>
2024-02-09 20:19:50 +0330

    Ready For Make First Image

---
+++ Src/ModirOnline.Log/ModirOnline.Log.Application/Interface/IDataBaseContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModirOnline.Log.Application.Interface
+{
+    internal interface IDataBaseContext
+    {
+    }
+}
---
+++ Src/ModirOnline.Log/ModirOnline.Log.Persistence/Context/DataBaseContext.cs
@@ -0,0 +1,28 @@
+﻿using ModirOnline.Log.Application.Interface;
+using ModirOnline.Log.Domain.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModirOnline.Log.Persistence.Context
+{
+    public class DataBaseContext: IDataBaseContext
+    {
+        private readonly IMongoDatabase _database;
+        private readonly string _collectionName;
+
+        public DataBaseContext(DatabaseSetting settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            _database = client.GetDatabase(settings.DatabaseName);
+            _collectionName = settings.CollectionName;
+            
+        }
+
+        public IMongoCollection<SysLog> SysLogs => _database.GetCollection<SysLog>(_collectionName);
+    }
+}
+
