88fc9ad9ddca413da557c230e713a726207d19f9
iman abdolahi <abdolahi.iman73@gmail.com>
2024-02-09 19:14:19 +0330

    First Commit

---
+++ Src/ModirOnline.Common/ModirOnline.Common/Dto/ApiResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModirOnline.Common.Dto
+{
+    public class ApiResult<T> where T : class
+    {
+        public bool IsSuccess { get; set; }
+        public int StatusCode { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public T Data { get; set; }
+    }
+    public class ApiResult
+    {
+        public bool IsSuccess { get; set; }
+        public int StatusCode { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
