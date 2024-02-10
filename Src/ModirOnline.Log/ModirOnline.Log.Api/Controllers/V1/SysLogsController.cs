ab7d7bdd9249a89323d8579abb478c3234356643
shahramteimoory <Darn.shan@yahoo.com>
2024-02-10 12:12:24 +0330

    Compltete Service SysLog Services

---
+++ Src/ModirOnline.Log/ModirOnline.Log.Api/Controllers/V1/SysLogsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModirOnline.Log.Api.Controllers.V1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class SysLogsController : ControllerBase
+    {
+        [HttpPost("InputSysLogsDto")]
+        public ActionResult CreateLog()
+        {
+            return null;
+        }
+    }
+}
---
+++ Src/ModirOnline.Log/ModirOnline.Log.Application/Services/SysLogsServices/ISysLogServices.cs
@@ -0,0 +1,86 @@
+﻿using ModirOnline.Common.Dto;
+using ModirOnline.Common.Dto.SysLogsDto;
+using ModirOnline.Common.Enums;
+using ModirOnline.Log.Application.Interface;
+using ModirOnline.Log.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModirOnline.Log.Application.Services.SysLogsServices
+{
+    public interface ISysLogServices
+    {
+        Task<ApiResult<IEnumerable<SysLog>>> GetSyslogs();
+        Task<ApiResult> GetSyslog(string id);
+        Task<ApiResult<IEnumerable<SysLog>>> GetSysLogsByClassName(string className);
+        Task<ApiResult<IEnumerable<SysLog>>> GetSysLogsByFunctionName(string functionName);
+        Task<ApiResult<IEnumerable<SysLog>>> GetSysLogsByLogType(LogType logType);
+        Task<ApiResult<IEnumerable<SysLog>>> InnerExeception();
+        Task<ApiResult> CreateSysLog(InputSysLogsDto req);
+        Task<ApiResult>DeleteSyslog(string id);
+        Task<ApiResult> DeleteSyslogs();
+    }
+    public class SysLogServices : ISysLogServices
+    {
+        private readonly IDataBaseContext _context;
+        public SysLogServices(IDataBaseContext context)
+        {
+            _context=context;
+        }
+        public Task<ApiResult> CreateSysLog(InputSysLogsDto req)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Task<ApiResult> DeleteSyslog(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResult> DeleteSyslogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResult> GetSyslog(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResult<IEnumerable<SysLog>>> GetSyslogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResult<IEnumerable<SysLog>>> GetSysLogsByClassName(string className)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResult<IEnumerable<SysLog>>> GetSysLogsByFunctionName(string functionName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResult<IEnumerable<SysLog>>> GetSysLogsByLogType(LogType logType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResult<IEnumerable<SysLog>>> InnerExeception()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
