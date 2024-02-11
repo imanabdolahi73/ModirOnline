e621d69af17b327490a7a5272865f144c1de4953
iman abdolahi <abdolahi.iman73@gmail.com>
2024-02-11 17:47:14 +0330

    Add Migration and update database ProductManagement

---
+++ Src/ModirOnline.Common/ModirOnline.Common/BaseInrerfaces/IBaseDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModirOnline.Common.BaseInrerfaces
+{
+    public interface IBaseDbContext
+    {
+        int SaveChanges(bool acceptAllChangesOnSuccess);
+        int SaveChanges();
+
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken());
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
+    }
+}
