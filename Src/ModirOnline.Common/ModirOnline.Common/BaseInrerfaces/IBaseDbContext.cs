543ccb204c9693f1fb1658b1fd97d704371374a2
iman abdolahi <abdolahi.iman73@gmail.com>
2024-02-11 18:11:05 +0330

    Add BaseCrudService and PaginatedList

--- Src/ModirOnline.Common/ModirOnline.Common/BaseInrerfaces/IBaseDbContext.cs
+++ Src/ModirOnline.Common/ModirOnline.Common/BaseInrerfaces/IBaseDbContext.cs
@@ -10,7 +10,6 @@ namespace ModirOnline.Common.BaseInrerfaces
     {
         int SaveChanges(bool acceptAllChangesOnSuccess);
         int SaveChanges();
-
         Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken());
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
     }
---
+++ Src/ModirOnline.Common/ModirOnline.Common/Utility/PaginatedList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModirOnline.Common.Utility
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            AddRange(items);
+        }
+
+        public bool HasPreviousPage => PageIndex > 1;
+
+        public bool HasNextPage => PageIndex < TotalPages;
+
+        public static PaginatedList<T> Create(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            int count = source.Count();
+            List<T> items = source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
